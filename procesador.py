e65d7e5ef9f5ff08b6355d06e80e56580e4d0009
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-08-19 16:35:08 -0400

    Update procesador.py

--- procesador.py
+++ procesador.py
@@ -1,57 +1,9 @@
 import psycopg2
 import faust
-from time import gmtime, strftime
+from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
+import json
 
-thread_pool = ThreadPoolExecutor(max_workers=1)
-
-app = faust.App(
-    "app1",
-    broker='kafka://-:-',
-    value_serializer='json',
-)
-test_topic = app.topic("-")
-historial_cam = []
-regCameras = set()
-
-@app.agent(test_topic)
-async def procesamiento(events):
-    async for event in events:
-        datosCamaras = {}
-        aux = {}
-        cnt = 0
-        count = event["lc_person"]["count"].split("|")
-        for i in event["lc_person"]["line_id"].split("|"):
-            aux1 = {}
-            if i != '':
-                if int(i) == 0:
-                    aux1['ins'] = count[cnt]
-                else:
-                    aux1['outs'] = count[cnt]
-                aux[cnt] = aux1
-                cnt += 1
-        datosCamaras[event["lc_person"]["camera_id"]] = aux
-        historial_cam.append(datosCamaras)
-        regCameras.add(event["lc_person"]["camera_id"])
-
-@app.timer(300.0)
-async def my_periodic_task():
-    app.loop.run_in_executor(thread_pool, insert_data)
-
-def preProcesamiento(registrosStream):
-    jirito = []
-    for camId in regCameras:
-        aux = list(e for e in registrosStream if camId in e)
-        if len(aux) != 0:
-            ins = 0
-            out = 0
-            for reg in aux:
-                ins += int(reg[camId][0]['ins'])
-                out += int(reg[camId][1]['outs'])
-            jirito.append((camId, strftime("%Y/%m/%d", gmtime()),strftime("%H:%M:%S", gmtime()),"laCatunga","-", "SioKE",ins,out))
-    return jirito
-
-# Funciones de conexion a la Base de datos
 param_dic = {
     "host": "-",
     "database": "-",
@@ -59,36 +11,104 @@ param_dic = {
     "password": "-"
 }
 
+app = faust.App(
+    "ETL",
+    broker='kafka://-:-',
+    value_serializer='json',
+)
+test_topic = app.topic("pruebascrack")
+
+thread_pool = ThreadPoolExecutor(max_workers=1)
+
+with open("camaras_info.json") as jsonFile:
+    jsonCamInfo = json.load(jsonFile)
+    jsonFile.close()
+
+listDisaggregatedRecords = []
+setCameraId = set()
+
+
+@app.agent(test_topic)
+async def streamUnbundler(events):
+    global listDisaggregatedRecords, setCameraId
+    async for event in events:
+        dictCamera = {}
+        auxDict = {}
+        listCounts = event["lc_person"]["count"].split("|")
+        for lineType in event["lc_person"]["line_id"].split("|"):
+            if lineType != '':
+                if int(lineType) == 0:  # Ingreso
+                    auxDict['ins'] = int(listCounts[int(lineType)])
+                else:  # Salida
+                    auxDict['outs'] = int(listCounts[int(lineType)])
+        dictCamera[event["lc_person"]["camera_id"]] = auxDict
+        listDisaggregatedRecords.append(dictCamera)
+        setCameraId.add(event["lc_person"]["camera_id"])
+
+
+@app.timer(90.0)
+async def loadTopostgreSQL():
+    global thread_pool
+    app.loop.run_in_executor(thread_pool, insert_data)
+
 def connect(params_dic):
-    """ Connect to the PostgreSQL database server """
     conn = None
     try:
         # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params_dic)
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-    print("Connection successful")
+    #print("Connection successful PostgreSQL")
     return conn
 
 
 def insert_data():
-    
-    global historial_cam
-    recordInsert = preProcesamiento(historial_cam)
 
-    sqlQuery = """INSERT INTO test_dk_crack(id_cc, fecha, hora, acceso_id, nombre_comercial_acceso, piso, ins, outs) VALUES(%s,%s,%s,%s,%s,%s,%s,%s);"""
+    global listDisaggregatedRecords, param_dic
+    listRecordInsert = recordGenerator(listDisaggregatedRecords)
+    listDisaggregatedRecords = []
+
+    if len(listRecordInsert) != 0:
+        print("Insertando data...")
+        sqlQuery = """INSERT INTO test_dk_crack(id_cc, fecha, hora, acceso_id, nombre_comercial_acceso, piso, ins, outs) VALUES(%s,%s,%s,%s,%s,%s,%s,%s);"""
+        try:
+            conn = connect(param_dic)
+            cur = conn.cursor()
+            for record in listRecordInsert:
+                cur.execute(sqlQuery, record)
+                conn.commit()
+            print(str(len(listRecordInsert))+" record inserted successfully")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+    else:
+        print("Sin data")
+
+def recordGenerator(recordStreams):
+    global setCameraId
+    listRecord = []
+    if len(recordStreams) != 0:
+        for camId in setCameraId:
+            listFiltered = list(e for e in recordStreams if camId in e)
+            if len(listFiltered) != 0:
+                ins = 0
+                outs = 0
+                for record in listFiltered:
+                    ins += int(record[camId]['ins'])
+                    outs += int(record[camId]['outs'])
+                listRecord.append(getInfoCam(camId)+(ins, outs))
+    return listRecord
+
+
+def getInfoCam(camId):
+    global jsonCamInfo
+    now = datetime.now()
+    date = now.strftime("%m/%d/%Y")
+    time = now.strftime("%H:%M:%S")
     try:
-        conn = connect(param_dic)
-        cur = conn.cursor()
-        for i in recordInsert:
-            cur.execute(sqlQuery, i)
-            conn.commit()
+        return(jsonCamInfo[str(camId)]['id_cc'], date, time, jsonCamInfo[str(camId)]['acceso_id'], jsonCamInfo[str(camId)]['nombre_comercial_acceso'], jsonCamInfo[str(camId)]['piso'])
+    except:
+        return("-1", date, time, "---", "---", "---")
 
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print("PostgreSQL connection is closed")
-            historial_cam=[]
