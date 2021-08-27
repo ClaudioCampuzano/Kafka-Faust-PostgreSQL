ca4889e7d94f622f0418da4fefc6098f06afe3d8
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-08-27 17:08:25 -0400

    Update stateCam.py

--- stateCam.py
+++ stateCam.py
@@ -4,30 +4,35 @@ import psycopg2
 from psycopg2 import extras
 from psycopg2 import sql
 
-
-
 app = faust.App(
     "status",
     broker='kafka://127.0.0.1:9092',
     value_serializer='json',
 )
+
+param_dic = {
+    "host": "192.168.0.127",
+    "database": "omia",
+    "user": "postgres",
+    "password": "Video2021feo$",
+     "port": "5432"
+}
+
+tableName='traza'
 topic = app.topic("analytics-omia")
 
 thread_pool = ThreadPoolExecutor(max_workers=None)
 camerasId = dict()
 
-param_dic = {
-    "host": "192.168.0.127",
-    "database": "dk_omia",
-    "user": "postgres",
-    "password": "Video2021feo$"
-}
-tableName='dev_visitantes_totales'
-
-@app.agent(topic)
+@app.agent(topic) 
 async def streamUnbundler(events):
     async for event in events:
-      camerasId[int(event["lc_person"]["camera_id"])] = event["@timestamp"]
+        """res = next((dicc for dicc in cameraStatus if dicc['id'] == event['lc_person']['camera_id']), None)
+        if res == None:
+            cameraStatus.append({'id': event['lc_person']['camera_id'],'timestamp': event['@timestamp']})
+        else:
+            res['timestamp'] = event['@timestamp']"""
+        camerasId[int(event["lc_person"]["camera_id"])] = event["@timestamp"]
       
 @app.timer(60.0)
 async def loadTopostgreSQL():
@@ -35,9 +40,9 @@ async def loadTopostgreSQL():
     app.loop.run_in_executor(thread_pool, insert_data)
     
 def insert_data():
-    global param_dic, camerasId, tableName
-    queryText = "UPDATE {table} SET timestamp=%s WHERE camara_id=$;"  
-    camInfo = [(k, v) for k, v in camerasId.items()]
+    global param_dic, tableName, camerasId
+    queryText = "UPDATE {table} AS t SET timestamp=e.times FROM (VALUES %s) AS e(id, times) WHERE e.id = t.camara_id;"
+    camInfo = [(str(k), str(v)) for k, v in camerasId.items()]
     try:
         conn = connect(param_dic)
         if conn is None:
@@ -45,10 +50,10 @@ def insert_data():
         cur = conn.cursor()
 
         sqlQuery = sql.SQL(queryText).format(table=sql.Identifier(tableName))
-        extras.execute_values(cur, sqlQuery.as_string(cur), camInfo)
+        extras.execute_values(cur, sqlQuery.as_string(cur), camInfo, template=None, page_size=100)
         conn.commit()
 
-        print("Inserting current data: "+str(cur.rowcount)+" records inserted successfully")
+        print("Updated: "+str(cur.rowcount)+" records successfully")
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
