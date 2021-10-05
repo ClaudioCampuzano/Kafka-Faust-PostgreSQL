bc294c1e80938c7686f18e81f56a908501e6606c
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-10-05 17:47:31 -0300

    Update flujo.py

--- flujo.py
+++ flujo.py
@@ -2,13 +2,12 @@
 import psycopg2
 import faust
 import json
-import pytz
-
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
-
 from psycopg2 import extras
 from psycopg2 import sql
+
+thread_pool = ThreadPoolExecutor(max_workers=None)
 
 param_dic = {
     "host": "192.168.0.127",
@@ -16,16 +15,15 @@ param_dic = {
     "user": "postgres",
     "password": "Video2021$"
 }
-tableName='dev_visitantes_totales'
+tableName='visitantes_totales'
 
 app = faust.App(
-    "ETL",
+    "ETL-flujo",
     broker='kafka://127.0.0.1:9092',
     value_serializer='json',
 )
-test_topic = app.topic("analytics-omia")
-
-thread_pool = ThreadPoolExecutor(max_workers=None)
+main_topic = app.topic("analytics-omia")
+timeToUpload = 60.0
 
 with open("camarasInfo_flujo.json") as jsonFile:
     jsonCamInfo = json.load(jsonFile)
@@ -35,50 +33,37 @@ listDisaggregatedRecords = []
 listRecordStandby = []
 setCameraId = set()
 
-guayaquil = pytz.timezone('America/Guayaquil')
-
-
-@app.agent(test_topic)
+@app.agent(main_topic)
 async def streamUnbundler(events):
-    global listDisaggregatedRecords, setCameraId
+    global listDisaggregatedRecords, setCameraId, jsonCamInfo
+
     async for event in events:
-        dictCamera = {}
-        auxDict = {}
-        listCounts = event["lc_person"]["count"].split("|")
-        for lineType in event["lc_person"]["line_id"].split("|"):
-            if lineType != '':
-                if int(lineType) == 0:  # Ingreso
-                    auxDict['ins'] = int(listCounts[int(lineType)])
-                else:  # Salida
-                    auxDict['outs'] = int(listCounts[int(lineType)])
-        dictCamera[event["lc_person"]["camera_id"]] = auxDict
-        listDisaggregatedRecords.append(dictCamera)
-        setCameraId.add(event["lc_person"]["camera_id"])
+        keysEvent = [*event]
+        if 'lc_person' in keysEvent:
+            dictCamera = {}
+            dictCamera['in'], dictCamera['out'], index  = 0, 0, 0
+            listCounts = [x for x in event['lc_person']['count'].split('|') if x]
+            for count in listCounts:
+                if index % 2 == 0:
+                    dictCamera['in'] += int(count)
+                else:
+                    dictCamera['out'] += int(count)
+                index += 1
 
+            dictCamera['camera_id'] = event['lc_person']['camera_id']
+            setCameraId.add(dictCamera['camera_id'])
+            listDisaggregatedRecords.append(dictCamera)
 
-@app.timer(300.0)
+@app.timer(timeToUpload)
 async def loadTopostgreSQL():
     global thread_pool
     app.loop.run_in_executor(thread_pool, insert_data)
 
-def connect(params_dic):
-    conn = None
-    try:
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params_dic)
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(end=" ")
-    #print("Connection successful PostgreSQL")
-    return conn
-
-
 def insert_data():
+    global param_dic, listRecordStandby, tableName
+    listRecordInsert = recordGenerator()
 
-    global listDisaggregatedRecords, param_dic, listRecordStandby, tableName
-    listRecordInsert = recordGenerator(listDisaggregatedRecords)
-    listDisaggregatedRecords = []
-
-    if len(listRecordInsert) != 0 or len(listRecordStandby) != 0:
+    if listRecordInsert:
         queryText = "INSERT INTO {table}(id_cc, fecha, hora, acceso_id, nombre_comercial_acceso, piso, ins, outs) VALUES %s;"
         try:
             conn = connect(param_dic)
@@ -90,34 +75,35 @@ def insert_data():
             extras.execute_values(cur, sqlQuery.as_string(cur), listRecordInsert+listRecordStandby)
             conn.commit()
 
-            print("Inserting current data: "+str(cur.rowcount)+" records inserted successfully")
-            if len(listRecordStandby)!= 0:
-                print("Of the above information, "+ str(len(listRecordStandby)) +" corresponds to old data") 
-                listRecordStandby = []
+            print(str(cur.rowcount)+" records inserted ("+str(len(listRecordStandby))+" correspond to the backup)")
+            listRecordStandby = []
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
             listRecordStandby += listRecordInsert
-            print("Saving information ("+str(len(listRecordInsert))+" records) for future use ("+str(len(listRecordStandby))+" total records in standby)")
+            print(str(len(listRecordStandby))+" records in standby")
         finally:
             if conn is not None:
                 conn.close()
     else:
         print("Whitout data")
 
-def recordGenerator(recordStreams):
-    global setCameraId
-    listRecord = []
-    if len(recordStreams) != 0:
+def recordGenerator():
+    global setCameraId, listDisaggregatedRecords
+    listRecordFlujo = []
+    listRecordAtributosIn = []
+    listRecordAtributosOut = []
+    if listDisaggregatedRecords:
         for camId in setCameraId:
-            listFiltered = list(e for e in recordStreams if camId in e)
-            if len(listFiltered) != 0:
-                ins = 0
-                outs = 0
+            listFiltered = [e for e in listDisaggregatedRecords if camId == e['camera_id']]
+            if listFiltered:
+                income, outflows = 0, 0
                 for record in listFiltered:
-                    ins += int(record[camId]['ins'])
-                    outs += int(record[camId]['outs'])
-                listRecord.append(getInfoCam(camId)+(ins, outs))
-    return listRecord
+                    income += int(record['in'])
+                    outflows += int(record['out'])
+                listRecordFlujo.append(getInfoCam(camId)+(income, outflows))
+    listDisaggregatedRecords.clear()
+    setCameraId.clear()
+    return listRecordFlujo
 
 
 def getInfoCam(camId):
@@ -129,3 +115,14 @@ def getInfoCam(camId):
         return(jsonCamInfo[str(camId)]['id_cc'], date, time, jsonCamInfo[str(camId)]['acceso_id'], jsonCamInfo[str(camId)]['nombre_comercial_acceso'], jsonCamInfo[str(camId)]['piso'])
     except:
         return("-1", date, time, "---", "---", "---")
+
+
+def connect(params_dic):
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params_dic)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(end=" ")
+    #print("Connection successful PostgreSQL")
+    return conn
